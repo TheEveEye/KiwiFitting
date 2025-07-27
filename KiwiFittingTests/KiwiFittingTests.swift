37537b9cfacce5a6f2e6917c839ca4496b16b355
SomeKiwi <cryfox01@gmail.com>
2025-07-27 13:06:48 +0200

    Initial Commit

---
+++ KiwiFittingTests/KiwiFittingTests.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiFittingTests.swift
+//  KiwiFittingTests
+//
+//  Created by Oskar on 7/27/25.
+//
+
+import Testing
+@testable import KiwiFitting
+
+struct KiwiFittingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
